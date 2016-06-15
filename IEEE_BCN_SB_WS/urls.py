cb0fcf65212db358478f6af3346f6479c0b7dbb7
Alejandro Agustin <alejandro.agustin@est.fib.upc.edu>
2016-06-15 19:35:28 +0200

    Web app created

--- IEEE_BCN_SB_WS/urls.py
+++ IEEE_BCN_SB_WS/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^polls/', include('polls.urls')),
     url(r'^web/', include('web.urls')),
 ]
--- polls/views.py
+++ polls/views.py
@@ -9,15 +9,15 @@ from polls.models import Choice, Question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'web/index.html', context)
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'web/detail.html', {'question': question})
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'web/results.html', {'question': question})
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -26,7 +26,7 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(request, 'web/detail.html', {
+        return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
         })
